c57e3d9acc1a88e7f75df32ed5c8f2959dcad931
颜经鑫 <459914911@qq.com>
2020-07-28 11:53:46 +0800

    初始commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AnimNotifyTool
+
+Developed with Unreal Engine 4
---
+++ Source/AnimNotifyTool/AnimNotifyToolGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AnimNotifyToolGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AAnimNotifyToolGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AAnimNotifyToolGameMode();
+};
+
+
+
